e4c349cc45b729915b4cce062472109cd9248dc2
Andrey <andreyproskura88@gmail.com>
2018-01-10 18:45:13 +0200

    created solution

---
+++ OpenWeather/OpenWeather/OpenWeatherPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace OpenWeather
+{
+    public partial class OpenWeatherPage : ContentPage
+    {
+        public OpenWeatherPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

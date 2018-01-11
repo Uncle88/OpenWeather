eaef31b2d472d9d3fd166b1035b64c05f4eafcea
Andrey <andreyproskura88@gmail.com>
2018-01-11 21:16:27 +0200

    bindable property in viewModel

--- OpenWeather/OpenWeather/ViewModels/OpenWeatherViewModel.cs
+++ OpenWeather/OpenWeather/ViewModels/OpenWeatherViewModel.cs
@@ -5,6 +5,148 @@ namespace OpenWeather.ViewModels
     {
         public OpenWeatherViewModel()
         {
+            City = "DNIPRO";
+        }
+
+        private string _city;
+        private string _temperature;
+        private DateTime _dateTimeRequest;
+        private string _wind;
+        private string _cloudiness;
+        private string _pressure;
+        private string _humidity;
+        private string _sunrise;
+        private string _sunset;
+        private string _geoCoord;
+
+        public string City
+        {
+            get { return _city; }
+            set
+            {
+                if (_city != value)
+                {
+                    _city = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Temperature
+        {
+            get { return _temperature; }
+            set
+            {
+                if (_temperature != value)
+                {
+                    _temperature = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public DateTime DateTimeRequest
+        {
+            get { return _dateTimeRequest; }
+            set
+            {
+                if (_dateTimeRequest != value)
+                {
+                    _dateTimeRequest = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Wind
+        {
+            get { return _wind; }
+            set
+            {
+                if (_wind != value)
+                {
+                    _wind = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Cloudiness
+        {
+            get { return _cloudiness; }
+            set
+            {
+                if (_cloudiness != value)
+                {
+                    _cloudiness = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Pressure
+        {
+            get { return _pressure; }
+            set
+            {
+                if (_pressure != value)
+                {
+                    _pressure = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Humidity
+        {
+            get { return _humidity; }
+            set
+            {
+                if (_humidity != value)
+                {
+                    _humidity = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Sunrise
+        {
+            get { return _sunrise; }
+            set
+            {
+                if (_sunrise != value)
+                {
+                    _sunrise = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Sunset
+        {
+            get { return _sunset; }
+            set
+            {
+                if (_sunset != value)
+                {
+                    _sunset = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string GeoCoord
+        {
+            get { return _geoCoord; }
+            set
+            {
+                if (_geoCoord != value)
+                {
+                    _geoCoord = value;
+                    OnPropertyChanged();
+                }
+            }
         }
     }
 }

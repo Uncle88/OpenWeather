8f6c9e5770caef2737afca7760ba1b7e134f94d8
Andrey <andreyproskura88@gmail.com>
2018-03-02 17:50:19 +0200

    added and registered dependency service for popUpmessage

---
+++ OpenWeather/OpenWeather/Services/PopUpDialog/IPopUpDialogService.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace OpenWeather.Services.PopUpDialog
+{
+    public interface IPopUpDialogService
+    {
+        void ShowMessage(string Message);
+    }
+}
---
+++ OpenWeather/OpenWeather/Services/PopUpDialog/PopUpDialogService.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace OpenWeather.Services.PopUpDialog
+{
+    public class PopUpDialogService : IPopUpDialogService
+    {
+        public void ShowMessage(string Message)
+        {
+            DependencyService.Get<IPopUpDialogService>().ShowMessage(Message);
+        }
+    }
+}

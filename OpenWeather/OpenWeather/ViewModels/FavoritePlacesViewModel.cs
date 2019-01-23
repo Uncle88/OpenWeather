f09145f45d4958c1c86bf7e59b202c58dfbe50f8
Andrey <andreyproskura88@gmail.com>
2019-01-24 00:32:54 +0200

    added FavoriteViewModel

---
+++ OpenWeather/OpenWeather/ViewModels/FavoritePlacesViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Plugin.Geolocator;
+
+namespace OpenWeather.ViewModels
+{
+    public class FavoritePlacesViewModel
+    {
+        public FavoritePlacesViewModel()
+        {
+        }
+
+        public async Task GetFavoriteForecast()
+        {
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 50;
+            var position = await locator.GetPositionAsync();
+        }
+    }
+}
